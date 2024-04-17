f37cb967a37a78634099fe7bfeeb53a7e3d5cff8
Satyam Mishra <sm6394243434@gmail.com>
2024-04-17 17:28:20 +0530

    Array2

---
+++ Array.js
@@ -0,0 +1,34 @@
+//Switch
+//============Mini App====================
+//let user = "Admin";
+// switch (user) {
+//   case "Admin":
+//     console.log("you have got full access to the website");
+//     break;
+//   case "Mentor":
+//     console.log("you have got course access");
+//     break;
+//   case "student":
+//     console.log("you have got access to purchase course student");
+//     break;
+//   default:
+//     console.log("you have normal user");
+// }
+
+// =====================Date==============
+
+// const now = new Date();
+// console.log(now.toString()); // toString is a inbuild Method
+// console.log(now.getDay());
+// console.log(now.getFullYear());
+// console.log(now.getTime());
+// console.log(now.getMinutes());
+// console.log(now.getHours());
+// console.log(now.getMonth());
+
+//=================Array==========
+
+let Name = ["Ram", "Satyam", "Puneet"];
+//console.log(name);
+Name.push("Aman");
+console.log(Name);
