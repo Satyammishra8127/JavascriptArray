64d7640cc934cf5d9c96d7e1e81eb3af92e19d7b
Satyam Mishra <sm6394243434@gmail.com>
2024-04-17 12:56:27 +0530

    Array

---
+++ demo.js
@@ -0,0 +1,41 @@
+// let username = "Satyam Mishra";
+// const number = "8127777102";
+//console.log(typeof username);
+// console.log(`
+
+//  My Name is ${username}
+//  My number is ${number}
+
+// `);
+//console.log(username.length);
+
+//console.log(username[7]);
+
+//=============Conditions===========
+
+// let ratting = 4;
+// if (ratting == 5) {
+//   console.log("5 Stars");
+// } else if (ratting == 4) {
+//   console.log("4 stars");
+// } else {
+//   console.log("will be proved");
+// }
+
+// condition ? exp(true):exp(false)
+
+// login = false;
+// login ? console.log("home page") : console.log("login first");
+
+// const Pi = Math.PI;
+// console.log(Pi);
+
+// ===================Mini App======================
+
+// const dream11 = "KOL vs RCB";
+// let rcb = "player";
+// let kol = "player";
+// kol = ["philip", "russel", "rohit"];
+// rcb = ["kohli", "faf", "maxvell"];
+// toss = rcb;
+// toss ? console.log(rcb) : console.log(kol);
